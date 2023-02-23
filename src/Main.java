184006fb51f71ab62a86bfc072172655e15018c8
goshik033 <sk.gosha@yandex.ru>
2023-02-23 17:48:43 +0300

    Написаны все методы на черновую

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
         Scanner in= new Scanner(System.in);
         printMenu();
         StepTracker stepTracker= new StepTracker();
+        for (int i =0; i<30;i++){
+            stepTracker.monthToData.get(0)[i]=i+9990;
+        }
         int userInput= in.nextInt();
         while(userInput!=0){
             if (userInput==1){
@@ -14,20 +18,34 @@ public class Main {
                 System.out.println("Введите день");
                 int day =in.nextInt();
                 stepTracker.inputSteps(steps,month,day);
-                stepTracker.printMas();
+                printMenu();
+                userInput = in.nextInt();
             }
-
-            printMenu();
-            userInput=in.nextInt();
+            if (userInput==2) {
+                stepTracker.printStatMenu();
+                int userIn = in.nextInt();
+                while (userIn != 0) {
+                    System.out.println("Введите месяц");
+                    String month = in.next();
+                    stepTracker.showStat(month, userIn);
+                    userIn = in.nextInt();
+                }
+                printMenu();
+                userInput = in.nextInt();
+            }
+            if (userInput==3){
+                System.out.println("Введите цель");
+                int goal = in.nextInt();
+                stepTracker.changeGoal(goal);
+                printMenu();
+                userInput = in.nextInt();
+            }
         }
-
     }
     public static void printMenu(){
-        System.out.println("1 - Вывести количество шагов за определенный день");
+        System.out.println("1 - Ввести количество шагов за определенный день");
         System.out.println("2 - Напечатать статистику за опредделенный месяц");
         System.out.println("3 - Изменить цель по количеству шагов");
         System.out.println("0 - Выйти из приложения");
-
     }
-
 }
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 
 public class StepTracker {
     HashMap<Integer,int[]> monthToData=new HashMap<Integer,int[]>();
+    int norm=10000;
     public StepTracker(){
         for (int i=0;i<12;i++){
             monthToData.put(i,createMassive());
@@ -23,6 +24,95 @@ public class StepTracker {
 
         }
 
+    }
+    public void changeGoal(int count){
+        norm=count;
+        System.out.println("Теперь цель "+norm);
+
+    }
+    public void showStat(String month, int userIn){
+        int monthInt=monthConverter(month);
+        switch(userIn){
+            case 1:
+                System.out.println(stepsSum(monthInt));
+                printStatMenu();
+                break;
+            case 2:
+                System.out.println(maxStepCount(monthInt));
+                printStatMenu();
+                break;
+            case 3:
+                System.out.println((float) stepsSum(monthInt)/30);
+                printStatMenu();
+                break;
+            case 5:
+                everyDaySteps(monthInt);
+                printStatMenu();
+                break;
+            case 4:
+                System.out.println((float) stepsSum(monthInt)/1200);
+                printStatMenu();
+                break;
+            case 6:
+                System.out.println((float) stepsSum(monthInt)/1200*50+" ккал");
+                printStatMenu();
+                break;
+            case 7:
+                System.out.println(bestSeri(monthInt));
+                printStatMenu();
+                break;
+            case 0:
+                break;
+
+        }
+
+
+    }
+    public void everyDaySteps(int month){
+        for (int i =0; i<30;i++) {
+            System.out.print(i+1+" день: "+monthToData.get(month)[i]+", ");
+        }
+    }
+    public int stepsSum(int month){
+        int summ=0;
+        for (int i =0; i<30;i++) {
+            summ+= monthToData.get(month)[i];
+        }
+        return summ;
+    }
+    public int maxStepCount(int month){
+        int maxx=0;
+        for (int i =0; i<30;i++) {
+            if (maxx< monthToData.get(month)[i])
+                maxx= monthToData.get(month)[i];
+        }
+        return maxx;
+    }
+    public int bestSeri(int month){
+        int count=0;
+        int maxxcount=0;
+        for (int i =0; i<30;i++) {
+            if (norm< monthToData.get(month)[i]){
+                count+=1;
+                if (count>=maxxcount)
+                    maxxcount=count;
+            }
+            else
+                count=0;
+        }
+        return count;
+
+
+    }
+    public void printStatMenu(){
+        System.out.println("1 - Общее количесвто шагов за месяц");
+        System.out.println("2 - Максимальное количество шагов за месяц");
+        System.out.println("3 - Среднее количество шагов за месяц");
+        System.out.println("4 - Пройденная дистанция в километрах");
+        System.out.println("5 - Количество пройденных шагов по дням");
+        System.out.println("6 - Количество сожженных калорий");
+        System.out.println("7 - Лучшая серия");
+        System.out.println("0 - Выход");
     }
 
     public int monthConverter(String month){
